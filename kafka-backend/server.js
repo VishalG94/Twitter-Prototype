2dac05d2a7a4a15437e65dfe5ff7e66bbb928925
VishalG94 <45498019+VishalG94@users.noreply.github.com>
2019-11-18 17:45:51 -0800

    models kafka

--- kafka-backend/server.js
+++ kafka-backend/server.js
@@ -1,62 +1,62 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-var connection = new require('./kafka/Connection');
-var mongoose = require("mongoose");
-var passport = require('passport');
+var connection = new require('./kafka/Connection')
+var mongoose = require('mongoose')
+var passport = require('passport')
 
-//topics files
-var signup = require('./services/signup.js');
-var login = require('./services/login.js');
+// topics files
+var signup = require('./services/signup.js')
+var login = require('./services/login.js')
 
-
-// mongoose.connect('mongodb+srv://admin:admin@grubhub-kdo0w.mongodb.net/test?retryWrites=true&w=majority', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
-
-mongoose.connect('mongodb+srv://nishit:nishit@cluster0-hvg0q.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(
+  'mongodb+srv://kiranbijjala:kiranbijjala@cluster0-xvhje.gcp.mongodb.net/test?retryWrites=true&w=majority',
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
-    // useMongoClient: true
-})
+  }
+)
 
-app.use(passport.initialize());
+// mongoose.connect('mongodb+srv://nishit:nishit@cluster0-hvg0q.mongodb.net/test?retryWrites=true&w=majority', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//     // useMongoClient: true
+// })
 
-function handleTopicRequest(topic_name, fname) {
-    //var topic_name = 'root_topic';
-    var consumer = connection.getConsumer(topic_name);
-    var producer = connection.getProducer();
-    console.log('server is running ' + topic_name);
-    consumer.on('message', function (message) {
-        console.log('message received for ' + topic_name + " ", fname);
-        console.log(JSON.stringify(message.value));
-        var data = JSON.parse(message.value);
+app.use(passport.initialize())
 
-        fname.handle_request(data.data, function (err, res) {
-            console.log('after handle' + res);
-            var payloads = [
-                {
-                    topic: data.replyTo,
-                    messages: JSON.stringify({
-                        correlationId: data.correlationId,
-                        data: res
-                    }),
-                    partition: 0
-                }
-            ];
-            producer.send(payloads, function (err, data) {
-                console.log(data);
-            });
-            return;
-        });
+function handleTopicRequest (topic_name, fname) {
+  // var topic_name = 'root_topic';
+  var consumer = connection.getConsumer(topic_name)
+  var producer = connection.getProducer()
+  console.log('server is running ' + topic_name)
+  consumer.on('message', function (message) {
+    console.log('message received for ' + topic_name + ' ', fname)
+    console.log(JSON.stringify(message.value))
+    var data = JSON.parse(message.value)
 
-    });
+    fname.handle_request(data.data, function (err, res) {
+      console.log('after handle' + res)
+      var payloads = [
+        {
+          topic: data.replyTo,
+          messages: JSON.stringify({
+            correlationId: data.correlationId,
+            data: res
+          }),
+          partition: 0
+        }
+      ]
+      producer.send(payloads, function (err, data) {
+        console.log(data)
+      })
+    })
+  })
 }
 
 // Add your TOPICs here
-//first argument is topic name
-//second argument is a function that will handle this topic request
+// first argument is topic name
+// second argument is a function that will handle this topic request
 
-handleTopicRequest("post_signup", signup);
-handleTopicRequest("post_login", login);
+handleTopicRequest('post_signup', signup)
+handleTopicRequest('post_login', login)
