c183b6ab010428fa42a65e216edda0d465597e86
VishalG94 <45498019+VishalG94@users.noreply.github.com>
2019-11-19 17:05:02 -0800

    Wirte Tweet

--- Frontend/src/components/WriteTweet/WriteTweet.js
+++ Frontend/src/components/WriteTweet/WriteTweet.js
@@ -2,81 +2,62 @@ import React from 'react'
 import { nominalTypeHack } from 'prop-types'
 import './WriteTweet.css'
 
-let WriteTweet = props => {
-  return (
-    <li href='#' class='list-group-item'>
-      <div class='row'>
-        <div class='col-sm-1'>
-          <img
-            src={require('../img/Twitternew.png')}
-            class='preview-img'
-            width='50'
-            height='50'
-            alt='profile pic'
-          />
-        </div>
-        {/* <div class='col-sm-1'></div> */}
-        <div class='col-sm-11'>
-          <div class='bg'>
-            <textarea
-              maxlength='280'
-              rows='1'
-              class='writetweet'
-              //   width='50%'
-              placeholder="What's happening  "
+class WriteTweet extends React.Component {
+  render () {
+    return (
+      <li href='#' class='list-group-item'>
+        <div class='row'>
+          <div class='col-sm-1'>
+            <img
+              src={require('../img/Twitternew.png')}
+              class='preview-img'
+              width='50'
+              height='50'
+              alt='profile pic'
             />
-            <i class='fa fa-photo fa-2x' />
-            <button
-              type='button'
-              style={{
-                float: 'right',
-                outline: 'none',
-                border: 'none',
-                backgroundColor: '#4285f4'
-              }}
-              class='btn btn-primary'
-            >
-              Tweet
-            </button>
           </div>
-
-          {/* <div class="md-form">
-            <textarea id="form7" class="md-textarea form-control" rows="3"></textarea>
-            <label for="form7">Material textarea</label>
+          <div class='col-sm-11'>
+            <div class='bg'>
+              <textarea
+                maxlength='280'
+                rows='1'
+                class='writetweet'
+                placeholder="What's happening  "
+              />
+              <br />
+              <br />
+              <i
+                class='fa fa-photo fa-2x'
+                onClick={() => this.fileUpload.click()}
+              >
+                <input
+                  type='file'
+                  ref={fileUpload => {
+                    this.fileUpload = fileUpload
+                  }}
+                  style={{ display: 'none' }}
+                  onChange={this.groupImgUpload}
+                />
+              </i>
+              <button
+                type='button'
+                style={{
+                  float: 'right',
+                  outline: 'none',
+                  border: 'none',
+                  backgroundColor: '#4285f4'
+                }}
+                class='btn btn-primary'
+              >
+                Tweet
+              </button>
             </div>
-             */}
-          {/* <div class="md-form">
-            <i class="fas fa-pencil-alt prefix"></i>
-            <textarea id="form10" class="md-textarea form-control" rows="3"></textarea>
-            <label for="form10">Icon Prefix</label>
-            </div>  */}
-
-          {/* <div style={{ color: 'black' }}>{props.tweetsDtls.description}</div> */}
-
-          <br />
-          {/* <img
-            class='rounded-circle'
-            style={{ borderRadius: '10px' }}
-            src={props.tweetsDtls.img}
-            width='500px'
-            height='250%'
-            alt='profile pic'
-          /> */}
-          {/*
-          <br />
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <i class='far fa-comment fa-2x' />&nbsp;<span style={{fontSize:'15px'}}>{props.tweetsDtls.comments}</span>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <i class='fas fa-retweet fa-2x' />&nbsp;<span style={{ fontSize:'15px'}}>{props.tweetsDtls.retweets}</span>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <i class='far fa-heart fa-2x' />&nbsp;<span style={{fontSize:'15px'}}>{props.tweetsDtls.likes}</span>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <i class='fas fa-arrow-up fa-2x' /> */}
+            <br />
+          </div>
         </div>
-      </div>
-    </li>
-  )
+      </li>
+    )
+  }
 }
 
 export default WriteTweet
