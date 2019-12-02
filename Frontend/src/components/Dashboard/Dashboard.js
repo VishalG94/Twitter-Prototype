05c9d14da3133bfbbad7ebd394ec6cf226934a96
Kiran Kumar Bijjala <kirankumar.bijjala@sjsu.edu>
2019-12-01 17:14:13 -0800

    updated dashboard

--- Frontend/src/components/Dashboard/Dashboard.js
+++ Frontend/src/components/Dashboard/Dashboard.js
@@ -1,25 +1,24 @@
 import React,{ Component } from 'react';
 import './Dashboard.css'
 import Profile from "../Profile/Profile";
-
+import LeftNavbar from '../LeftNavbar/LeftNavbar'
 import cookies from 'react-cookies';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import Redirect from "react-router-dom/es/Redirect";
 import connect from "react-redux/es/connect/connect";
 import Messages from '../Messages/Messages';
 import Tweet from '../Tweet/Tweet'
-import UserTweets from '../UserTweets/UserTweets'
-import UserLikes from '../UserTweets/UserLikes'
-import LeftNavbar from '../LeftNavbar/LeftNavbar';
-
+import Barday from './Barday'
+import GraphLikes from './GraphLikes'
+import GraphRetweet from './GraphRetweet'
  class Dashboard extends Component{
 
     constructor(props){
         super(props);
 
         this.state = {
-            userid : this.props.user._id,
-            component:'Tweets'
+            //userid : cookies.load('userid')
+            userid : this.props.user._id
         }
     }
 
@@ -29,32 +28,60 @@ import LeftNavbar from '../LeftNavbar/LeftNavbar';
     {
             return (
 
-
-                
                 <div>
-
-                <div className="col-sm-2">
-                    <LeftNavbar />
+                <div>
+                  <div className='row'>
+                    <div className='col-sm-2'>
+                      <LeftNavbar />
+                    </div>
+                     <div class='split-center-dashboard'>
+                     <div className='row'>
+                    <div class='col-sm-4'>
+                    <GraphLikes></GraphLikes> 
+                    </div>
+                    <div class='col-sm-4'>
+                    <GraphRetweet></GraphRetweet>
+                    </div>
+                    </div>
+                    <div className='row'>
+                    <Barday></Barday>
+                     </div>
+                    </div>
+                    <div className='col-sm-1' />
+                  </div>
                 </div>
+              </div>
+                // <div>
+                //     {/* <Navbarhome/> */}
+                //     <div className="container-dashboard">
+                //         <nav>
+                //             <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                //                 <a className={ "nav-item nav-link " + ((this.props.match.params.page === "inbox") && ("active"))  }><Link to={"/Dashboard/inbox/" + this.props.user._id}>Messages</Link></a>
+                //                 <a className={ "nav-item nav-link " + ((this.props.match.params.page === "tweets") && ("active"))  }><Link to={"/Dashboard/mytrips/" + this.props.user._id}>My Tweets</Link></a>
+                //                 <a className={ "nav-item nav-link " + ((this.props.match.params.page === "profile") && ("active"))  }><Link to={"/Dashboard/profile/" + this.props.user._id}>Profile</Link></a>
+                //                 <a className={ "nav-item nav-link " + ((this.props.match.params.page === "retweets") && ("active"))  }><Link to={"/Dashboard/account/" + this.props.user._id}>Retweets</Link></a>
+                //             </div>
+                //         </nav>
+                //         <div className="tab-content" id="nav-tabContent">
+                //             { (this.props.match.params.page === "inbox") && (<div className="tab-pane fade show active" id="nav-Inbox" role="tabpanel"
+                //                  aria-labelledby="nav-Inbox-tab"><Messages/>
+                //             </div>) }
+                //             { (this.props.match.params.page === "tweets") && (<div className="tab-pane fade show active" id="nav-trips" role="tabpanel"
+                //                  aria-labelledby="nav-trips-tab"><Tweet userid={this.props.user._id} />
+                //             </div>)}
+                //             { (this.props.match.params.page === "profile") && (<div className="tab-pane fade show active" id="nav-profile" role="tabpanel"
+                //                  aria-labelledby="nav-profile-tab"><Profile userid={this.props.user._id}/>
+                //             </div>)}
+                //             {/* { (this.props.match.params.page === "account") && (<div className="tab-pane fade show active" id="nav-Account"role="tabpanel"
+                //                 aria-labelledby="nav-profile-tab"><Retw userid={this.props.user._id}/>
+                //             </div>)} */}
+                //         </div>
+                //     </div>
 
-                <div class='split-center_new_dash'>
-                <nav class="navbar navbar">
-                            
-                            <div class="navbar-header">
-                            </div>
 
-                            <ul style={{width:"100%" }} class="nav navbar-nav">
 
-                                <li style={{width:"25%"}}> <a id="Tweets" onClick={this.selectComponent} style={{textAlign:"center",borderRadius:"0px", borderRight:'none',color : "black"}} href="/userprofile/tweets" class="list-group-item">Tweets</a></li>
-                                <li style={{width:"25%"}}> <a style={{textAlign:"center", borderRadius:"0px",borderRight:'none', color : "black"}} href="/followers" class="list-group-item">Followers</a></li>
-                                <li style={{width:"25%"}}> <a style={{textAlign:"center", borderRadius:"0px",borderRight:'none', color : "black"}} href="/graph" class="list-group-item">Graph</a></li>
-                                <li style={{width:"25%"}}> <a id="Likes" onClick={this.selectComponent} style={{textAlign:"center", borderRadius:"0px", color : "black"}} href="/userprofile/likes" class="list-group-item">Likes</a></li>
-                                
-                            </ul>
-
-                    </nav>
-                    </div>
-                    </div>
+                    
+                // </div>
             )
          }
     }
